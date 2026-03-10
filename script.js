edc4f1e6a43769397bb2dc3315f895e5ea4835ff
Jitendra Kumar Chauhan <jitendra.chauhan@abes.ac.in>
2026-03-10 10:49:01 +0530

    DOM

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
-console.log("Hello World");
-document.write("Hello World");
+// console.log("Hello World");
+// document.write("Hello World");
+
+const result=document.getElementById("para");
+result.innerHTML="Hello World";
+// document.write(result);
+
+const x=document.getElementsByClassName("p1");
+console.log(x);
+x[0].innerHTML="Jitendra";
+x[1].innerHTML="Chauhan";
